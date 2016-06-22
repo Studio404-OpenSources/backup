21c2efad7d11875e734c4d445b9b58d5b3dc1841
giorgi <opensource@404.ge>
2016-06-22 06:36:14 -0500

    add array to class method

--- backup.php
+++ backup.php
@@ -2,29 +2,103 @@
 namespace studio404;
 
 class backup{
-	function __construct($main_options){
-		$this->shell_folder = $main_options['shell_folder'];
-		$this->destination_folder = $main_options['destination_folder'];
+	function __construct($options){
+		$secondary = array(
+			"css"=>array(
+				"addlink"=>array(
+					"margin"=>"10px",
+					"padding"=>"10px 20px",
+					"background-color"=>"#f2f2f2",
+					"text-decoration"=>"none",
+					"color"=>"#555555",
+					"text-align"=>"center",
+					"float"=>"left"
+				)
+			),
+			"lang"=>array(
+				"addlink"=>"დამატება",
+				"errorMsg"=>"მოხდა შეცდომა !"
+			)
+		);
+		$this->option = $options + $secondary;
+		$this->post_request();
 	}
 
 	public function load(){
-		$backup_path = __DIR__."/index.php";
-		$archive_name = date("H-i-s").".tgz";
-		$this->tgz($backup_path, $archive_name);
+		echo $this->backup_table();
+	}
+
+	private function backup_table(){
+		$content = sprintf(
+			'<a href="%s" style="%s">%s</a>', 
+			$this->option['slug'],
+			$this->arrayToStyle($this->option['css']['addlink']),
+			$this->option['lang']['addlink']
+		);
+		return $content;
+	}
+
+	private function post_request(){
+		// $backup_path = __DIR__."/index.php";  $archive_name = date("H-i-s").".tgz";
+		if($this->requests("POST","bpath") && $this->requests("POST","aname")){
+			$backup_path = sprintf(
+				'%s',
+				$this->requests("POST","bpath")
+			);
+
+			$archive_name = sprintf(
+				'%s.tgz',
+				$this->requests("POST","aname")
+			);
+			$this->tgz($backup_path, $archive_name);
+		}
 	}
 
 	private function tgz($backup_path, $archive_name){
 		if((is_dir($backup_path) || is_file($backup_path)) &&  !empty($archive_name)){
 			$command = sprintf(
-				"sh %s/b.sh %s %s %s %s",
-				$this->shell_folder, 
+				'sh %s/b.sh %s %s %s %s',
+				$this->option['shell_folder'], 
 				$backup_path, 
-				$this->destination_folder, 
+				$this->option['destination_folder'], 
 				date("d-m-Y"), 
 				$archive_name
 			);
 			shell_exec($command);
 		}
 	}
+
+	private function requests($type,$item){
+		if($type=="POST" && isset($_POST[$item])){
+			return filter_input(INPUT_POST, $item);
+		}else if($type=="GET" && isset($_GET[$item])){
+			return filter_input(INPUT_GET, $item);
+		}else{
+			return '';
+		}
+	}
+
+	private function arrayToStyle($array){
+		$output = '';
+		try{
+			if(is_array($array)){				
+				$output = implode('; ', array_map(
+					function ($v, $k) { 
+						return sprintf("%s:%s", $k, $v); 
+					},
+					$array,
+					array_keys($array)
+				));
+			}
+		}catch(Exception $e){
+			$this->outMessage = sprintf(
+				'%s%s', 
+				$this->option['lang']['errorMsg'],
+				$e
+			);
+		}
+		return $output;
+	}
+
 }
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-@include "backup.php"; 
+header("Content-type: text/html; charset=utf-8");
 
+
+@include "backup.php"; 
 $main_options = array(
 	"shell_folder"=>"_shellx",
-	"destination_folder"=>__DIR__."/_temp/backups"
+	"destination_folder"=>"_temp/backups",
+	"slug"=>"index.php"
 );
 
 $backup = new studio404\backup($main_options);
