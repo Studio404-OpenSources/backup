1b1eb07cff47458a72374441ec477c0437823418
giorgi <opensource@404.ge>
2016-06-23 03:22:24 -0500

    modify file structure

--- backup.php
+++ backup.php
@@ -6,21 +6,45 @@ class backup{
 		$secondary = array(
 			"css"=>array(
 				"addlink"=>array(
-					"margin"=>"10px",
+					"margin"=>"10px 5px",
 					"padding"=>"10px 20px",
 					"background-color"=>"#f2f2f2",
 					"text-decoration"=>"none",
 					"color"=>"#555555",
 					"text-align"=>"center",
 					"float"=>"left"
+				),
+				"table"=>array(
+					"margin"=>"10px 5px",
+					"padding"=>"0px",
+					"width"=>"calc(100% - 10px)",
+					"float"=>"left",
+					"border"=>"solid 1px #f2f2f2"
+				),
+				"table.tr.head"=>array(
+					"margin"=>"0px",
+					"padding"=>"10px",
+					"background-color"=>"#cccccc",
+					"color"=>"white"
 				)
-			),
-			"lang"=>array(
-				"addlink"=>"დამატება",
-				"errorMsg"=>"მოხდა შეცდომა !"
 			)
 		);
 		$this->option = $options + $secondary;
+		$this->shell_path = sprintf(
+			'%s/%s',
+			$this->option['project_path'],
+			'shellx'
+		);
+		$this->backup_dir = sprintf(
+			'%s/%s',
+			$this->option['project_path'],
+			'backups'
+		);
+		$this->json_path = sprintf(
+			'%s/%s',
+			$this->option['project_path'],
+			'json'
+		);
 		$this->post_request();
 	}
 
@@ -31,17 +55,51 @@ class backup{
 	private function backup_table(){
 		$content = sprintf(
 			'<a href="%s" style="%s">%s</a>', 
-			$this->option['slug'],
+			$this->option['slug']['addlink'],
 			$this->arrayToStyle($this->option['css']['addlink']),
 			$this->option['lang']['addlink']
 		);
+
+		$content .= sprintf(
+			'<table style="%s" cellspaceing="10" cellpadding="10">
+			<tr style="%s">
+				<td>დასახელება</td>
+				<td>რეზერვი</td>
+				<td>თარიღი</td>
+				<td>მოქმედება</td>
+			</tr>
+			<tr>
+				<td>ფოტო</td>
+				<td>photoes/</td>
+				<td>20-07-2016</td>
+				<td><a href="">წაშლა</a> / <a href="">ჩამოტვირთვა</a></td>
+			</tr>
+			</table>',
+			$this->arrayToStyle($this->option['css']['table']), 
+			$this->arrayToStyle($this->option['css']['table.tr.head'])
+		);
+		$this->load_tgzs();
+
 		return $content;
+	}
+
+	private function load_tgzs(){
+		$command = 'ls '.$this->json_path.' 2>&1';
+		$shell = shell_exec($command);
+		$files = explode(".json", $shell);
+		$files = array_map('trim', $files);
+		
+		foreach ($files as $val) {
+			if(!empty($val) && $val != ""){
+				echo $val.".json<br />";
+			}
+		}
 	}
 
 	private function post_request(){
 		// $backup_path = __DIR__."/index.php";  $archive_name = date("H-i-s").".tgz";
 		if($this->requests("POST","bpath") && $this->requests("POST","aname")){
-			$backup_path = sprintf(
+			$backup = sprintf(
 				'%s',
 				$this->requests("POST","bpath")
 			);
@@ -50,17 +108,17 @@ class backup{
 				'%s.tgz',
 				$this->requests("POST","aname")
 			);
-			$this->tgz($backup_path, $archive_name);
+			$this->tgz($backup, $archive_name);
 		}
 	}
 
-	private function tgz($backup_path, $archive_name){
-		if((is_dir($backup_path) || is_file($backup_path)) &&  !empty($archive_name)){
+	private function tgz($backup, $archive_name){
+		if((is_dir($backup) || is_file($backup)) &&  !empty($archive_name)){
 			$command = sprintf(
 				'sh %s/b.sh %s %s %s %s',
-				$this->option['shell_folder'], 
-				$backup_path, 
-				$this->option['destination_folder'], 
+				$this->shell_path, 
+				$backup, 
+				$this->backup_dir, 
 				date("d-m-Y"), 
 				$archive_name
 			);
--- index.php
+++ index.php
@@ -4,9 +4,14 @@ header("Content-type: text/html; charset=utf-8");
 
 @include "backup.php"; 
 $main_options = array(
-	"shell_folder"=>"_shellx",
-	"destination_folder"=>"_temp/backups",
-	"slug"=>"index.php"
+	"project_path"=>"_temp",
+	"slug"=>array(
+		"addlink"=>"index.php?addbackup=true"
+	),
+	"lang"=>array(
+		"addlink"=>"დამატება",
+		"errorMsg"=>"მოხდა შეცდომა !"
+	)
 );
 
 $backup = new studio404\backup($main_options);
